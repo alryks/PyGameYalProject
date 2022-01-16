c7c87dfebfda711d841e4e40e0e0a39a70e67c88
nikskl <nikskl999@gmail.com>
2022-01-16 23:27:19 +0300

    Added whole mechanics of the spaceship

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ import shutil
 from PIL import Image
 
 import random
+import math
+
+'''
+CONSTANTS
+'''
+
+pi = math.pi
 
 '''
 Functions for images
@@ -54,10 +61,11 @@ def scale_image(name, size=1.0):
         sys.exit()
     img = Image.open(fullname)
     img_width, img_height = img.size
-    img_new = img.resize((int(img_width * size), int(img_height * size)))
+    img_scale = img.resize((int(img_width * size), int(img_height * size)))
     to_save = os.path.join(os.path.join("img", "temp"), name)
     create_temp()
-    img_new.save(to_save)
+    img_scale.save(to_save)
+    img.close()
 
 
 # FPS counter
@@ -73,16 +81,20 @@ def update_fps():
 class Spaceship(pygame.sprite.Sprite):
     def __init__(self, *group):
         super().__init__(*group)
-        scale_image("spaceship.png", size=width / 12 / 1000)
-        sprite_img = load_image(os.path.join("temp", "spaceship.png"))
-        self.image = sprite_img
+        scale_image("spaceship.png", size=width / 10 / 1000)
+        self.sprite_img = load_image(os.path.join("temp", "spaceship.png"))
+        self.image = self.sprite_img
         self.rect = self.image.get_rect()
         self.rect.x = width // 2 - self.rect.width // 2
         self.rect.y = height // 2 - self.rect.height // 2
+        self.width = self.rect.width
 
     def update(self, *args):
-        self.rect.x = args[0]
-        self.rect.y = args[1]
+        self.image = pygame.transform.rotate(self.sprite_img, args[2])
+        self.rect = self.image.get_rect()
+        d = (self.rect.width - self.width) // 2
+        self.rect.x = args[0] - d
+        self.rect.y = args[1] - d
 
 
 if __name__ == "__main__":
@@ -90,7 +102,7 @@ if __name__ == "__main__":
     pygame.display.set_caption('not enough SPACE')
     size = pygame.display.get_desktop_sizes()[0]
     # size = width, height = size[0] // 2, size[1] // 2
-    size = width, height = 1920, 1080
+    size = width, height = 1280, 720
     flags = DOUBLEBUF
     screen = pygame.display.set_mode(size, flags)
     screen.set_alpha(None)
@@ -99,10 +111,15 @@ if __name__ == "__main__":
 
     all_sprites = pygame.sprite.Group()
     spaceship = Spaceship(all_sprites)
-    v = 0
+    v = [0, 0]
     a = 0
     x = spaceship.rect.x
     y = spaceship.rect.y
+
+    aw = 0
+    w = 0
+    angle = 0
+    k = 1
 
     ''
 
@@ -114,25 +131,41 @@ if __name__ == "__main__":
         screen.fill('#111122')
         screen.blit(update_fps(), (width - 40, 10))
 
-        for event in pygame.event.get():
+        events = pygame.event.get()
+        for event in events:
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    a = height // (fps / 4)
+                    aw = height / fps * 15 / (2 * pi) * 360
                 elif event.key == pygame.K_RIGHT:
-                    a = -height // (fps / 4)
+                    aw = -height / fps * 15 / (2 * pi) * 360
+                elif event.key == pygame.K_DOWN:
+                    a = -height / fps
+                elif event.key == pygame.K_UP:
+                    a = height / fps
             if event.type == pygame.KEYUP:
-                a = 0
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    aw = 0
+                elif event.key == pygame.K_DOWN or event.key == pygame.K_UP:
+                    a = 0
 
         ''
 
         all_sprites.draw(screen)
 
-        v += a * clock.tick() / 1000
-        y += v
+        tick = clock.tick()
 
-        all_sprites.update(int(x), int(y))
+        w += aw * tick / 1000
+        angle += w * tick / 1000
+        angle_rad = angle / 360 * 2 * pi
+
+        v = [v[0] - a * tick / 1000 * math.sin(angle_rad),
+             v[1] - a * tick / 1000 * math.cos(angle_rad)]
+        x += v[0]
+        y += v[1]
+
+        all_sprites.update(int(x), int(y), angle)
 
         ''
 
